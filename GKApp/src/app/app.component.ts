817e1a55b795f4764bc4510c8772138c3946fb87
Gnana Sekhar <gnanasekhark996@gmail.com>
2020-06-21 19:48:28 -0400

    First sample application

--- GKApp/src/app/app.component.ts
+++ GKApp/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'GKApp',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   welcome = 'welcome to GKApp';
+  displayName = false;
+  constructor(){
+    setTimeout(()=>{
+      this.displayName = true;
+    },2000)
+  }
+  ngOnInit(){
+    
+  }
 }
